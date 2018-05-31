bd56290976e6b6c0c1686cfaca34d6c1feff36c4
Petri Saarilammi <github@saarilammi.com>
2018-05-31 19:23:38 +0300

    .net 2.1 upgrade

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM microsoft/dotnet:2.0-runtime
+FROM microsoft/dotnet:2.1-aspnetcore-runtime
 
 # set up network
 ENV ASPNETCORE_URLS http://+:8080
@@ -7,7 +7,7 @@ RUN apt-get update
 RUN apt-get upgrade -y
 
 RUN mkdir /app
-COPY src/ArkNotifications/bin/Release/netcoreapp2.0/publish/ /app
+COPY src/ArkNotifications/bin/Release/netcoreapp2.1/publish/ /app
 COPY src/ArkNotifications/appsettings.json /app
 WORKDIR /app
 COPY run-app.sh /app
--- src/ArkNotifications/Program.cs
+++ src/ArkNotifications/Program.cs
@@ -7,13 +7,13 @@ namespace ArkNotifications
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
-        public static IWebHost BuildWebHost(string[] args) =>
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
             .UseUrls("http://+:8080")
-            .UseStartup<Startup>()
-            .Build();
+            .UseStartup<Startup>();
     }
 }
 
