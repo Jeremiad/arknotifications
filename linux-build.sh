1e83d5a06a8ee181d14ccecf59015c19c51a3955
Petri Saarilammi <github@saarilammi.com>
2019-02-14 19:01:07 +0200

    docker cmd remove

--- linux-build.sh
+++ linux-build.sh
@@ -6,5 +6,5 @@ name=arknotifications_$(date +%Y%m%d)-$(git rev-parse --verify --short HEAD)
 dotnet restore src/ArkNotifications && dotnet publish -c Release src/ArkNotifications
 
 docker build -t $name .
-docker run -h ark1 -dt $name /app/run-app.sh
-docker run -h ark2 -dt $name /app/run-app.sh
+docker run -h ark1 -dt $name
+docker run -h ark2 -dt $name
